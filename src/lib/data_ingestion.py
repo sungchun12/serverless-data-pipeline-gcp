0d349db376cea18b53cff0aef36f65571e32d148
Sung Won Chung <sungwon.chung@slalom.com>
2019-02-26 23:09:41 -0600

    moved function_handler code to lib modules

---
+++ src/lib/data_ingestion.py
@@ -0,0 +1,105 @@
+"""
+Add a description here
+"""
+
+#built in python modules
+import os
+from datetime import datetime
+#gcp modules
+from gcloud import storage
+from google.cloud import bigquery
+import pandas_gbq as gbq
+from google.auth import compute_engine
+#api module
+from sodapy import Socrata
+#pandas dataframe module
+import pandas as pd
+
+
+#%%
+#create pandas dataframe
+def create_results_df():
+    """Create a dataframe based on JSON from the Chicago traffic API"""
+    try:
+        # First 2000 results, returned as JSON from API / converted to Python list of
+        # dictionaries by sodapy.
+        # Unauthenticated client only works with public data sets. Note 'None'
+        # in place of application token, and no username or password:
+        data_client = Socrata("data.cityofchicago.org", None)
+        results = data_client.get("8v9j-bter", limit=2000) #unique id for chicago traffic data
+
+        # Convert to pandas DataFrame
+        results_df = pd.DataFrame.from_records(results)
+        print("Successfully created a pandas dataframe!")
+        
+        return results_df
+    except Exception as e:
+        print("Failure to create a pandas dataframe :(")
+        raise e
+
+#%%
+#function upload data to cloud storage
+def upload_raw_data_gcs(results_df, bucket_name):
+    """Upload dataframe into google cloud storage bucket"""
+    try: 
+        # Write the DataFrame to GCS (Google Cloud Storage)
+        storage_client = storage.Client.from_service_account_json('service_account.json') #authenticate service account
+        bucket = storage_client.bucket(bucket_name) #capture bucket details
+        results_df.to_csv('traffic_' + _getToday() + '.csv') #convert dataframe to csv file type
+        source_file_name = 'traffic_' + _getToday() + '.csv' #create the file name
+        blob = bucket.blob(os.path.basename(source_file_name)) #define the path to the file
+        blob.upload_from_filename(source_file_name) #upload to bucket
+        print("Successfully uploaded csv file into {}".format(bucket))
+    except Exception as e:
+        print("Failure to upload data to google cloud storage bucket :(")
+        raise e
+
+#%%
+#https://stackoverflow.com/questions/21886742/convert-pandas-dtypes-to-bigquery-type-representation
+#https://stackoverflow.com/questions/44953463/pandas-google-bigquery-schema-mismatch-makes-the-upload-fail
+#http://pbpython.com/pandas_dtypes.html
+    
+def convert_schema(results_df, schema_dict):
+    """Converts data types in dataframe to match BigQuery destination table"""
+    for k, v in schema_dict.items(): #for each column name in the dictionary, convert the data type in the dataframe
+        results_df[k] = results_df[k].astype(v)
+    results_df_transformed = results_df
+    print("Updated schema to match BigQuery destination table")
+    return results_df_transformed
+
+def check_nulls(results_df_transformed):
+    """Checks if there are any nulls in the columns"""
+    try:
+        null_columns = []
+        check_bool = results_df_transformed.isnull().any() #returns a boolean True/False for every column in dataframe if it contains nulls
+        for k, v in check_bool.items(): #for each column in check_bool having nulls, print the name of the column
+            if check_bool[v] == False:
+                null_columns.append(k)
+        print("These are the null columns: {}".format(null_columns))
+        return null_columns
+    except Exception as e:
+        print(e)
+
+def check_null_outliers(null_columns, nulls_expected):
+    """Checks if there are any outlier nulls in the columns"""
+    try:
+        null_outliers=[] #empty list to collect list of columns that are not expected to be null
+        #if any columns in the nulls expected mismatch the nulls collected, append to null_outliers
+        if any(x not in nulls_expected for x in null_columns): 
+            null_outliers.append(x)
+        print("These are the outlier null columns: {}".format(null_outliers))
+        return null_outliers
+    except Exception as e:
+        print(e)
+
+#%%
+#figure out the nullable vs. required mode schema mismatch
+def upload_to_gbq(results_df_transformed, project_id, dataset_name, table_name):
+    """Uploads data into bigquery and appends if data already exists"""
+    try:
+        gbq.to_gbq(results_df_transformed, dataset_name+"."+table_name, project_id, 
+                   if_exists='append', location = 'US', progress_bar=False)
+        print('Data uploaded into project: {0}, dataset: {1}, table: {2}'.format(project_id, dataset_name, table_name))
+    except Exception as e:
+        print("Failure to upload data to Bigquery :(")
+        raise e
---
+++ src/lib/infrastructure_setup.py
@@ -0,0 +1,116 @@
+"""
+Add a description here
+"""
+
+#built in python modules
+import os
+from datetime import datetime
+#gcp modules
+from gcloud import storage
+from google.cloud import bigquery
+import pandas_gbq as gbq
+from google.auth import compute_engine
+#api module
+from sodapy import Socrata
+#pandas dataframe module
+import pandas as pd
+
+def create_bucket(bucket_name):
+    """Detects whether or not a new bucket needs to be created"""
+    client = storage.Client.from_service_account_json('service_account.json') #authenticate service account
+    bucket = client.bucket(bucket_name) #capture bucket details
+    bucket.location = 'US-CENTRAL1' #define regional location
+    if not bucket.exists(): #checks if bucket doesn't exist
+        bucket.create()
+        print("Created a new bucket: {0}".format(bucket_name))
+    else:
+        print("Bucket already exists")
+
+
+#%%
+def dataset_exists(client, dataset_reference):
+    """Return if a table exists.
+
+    Args:
+        client (google.cloud.bigquery.client.Client):
+            A client to connect to the BigQuery API.
+        table_reference (google.cloud.bigquery.table.TableReference):
+            A reference to the table to look for.
+
+    Returns:
+        bool: ``True`` if the table exists, ``False`` otherwise.
+    """
+    from google.cloud.exceptions import NotFound
+
+    try:
+        client.get_dataset(dataset_reference)
+        return True
+    except NotFound:
+        return False
+
+
+#%%
+def table_exists(client, table_reference):
+    """Return if a table exists.
+
+    Args:
+        client (google.cloud.bigquery.client.Client):
+            A client to connect to the BigQuery API.
+        table_reference (google.cloud.bigquery.table.TableReference):
+            A reference to the table to look for.
+
+    Returns:
+        bool: ``True`` if the table exists, ``False`` otherwise.
+    """
+    from google.cloud.exceptions import NotFound
+
+    try:
+        client.get_table(table_reference)
+        return True
+    except NotFound:
+        return False
+
+
+#%%
+#https://cloud.google.com/bigquery/docs/python-client-migration#update_a_table
+def create_dataset_table(dataset_name, table_name, table_desc, schema, partition_by):
+    """Detects whether or not a new dataset and/or table need to be created"""
+    #setup the client
+    bigquery_client = bigquery.Client()
+
+    # Create a DatasetReference using a chosen dataset ID.
+    dataset_ref = bigquery_client.dataset(dataset_name) # The project defaults to the Client's project if not specified.
+
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_ref)
+
+    # Specify the geographic location where the dataset should reside.
+    dataset.location = "US"
+    
+    # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    if dataset_exists(bigquery_client, dataset_ref) == False: #checks if dataset not found
+        dataset = bigquery_client.create_dataset(dataset)  # API request
+        print("Created new dataset")
+    else:
+        print("Dataset already exists")
+    
+    #Create an empty table
+    table_ref = dataset_ref.table(table_name) #construct a full table object to send to the api
+    
+    if table_exists(bigquery_client, table_ref) == False: #checks if table not found
+        table = bigquery.Table(table_ref, schema=schema)
+        table.time_partitioning = bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY, #day is the only supported type for now
+            field=partition_by)  # name of column to use for partitioning
+        table = bigquery_client.create_table(table)  # API request
+        assert table.table_id == table_name #checks if table_id matches table_name
+        
+        #update the table description
+        table.description = table_desc
+        table = bigquery_client.update_table(table, ['description'])  # API request
+        assert table.description == table_desc #checks if table description matches the update
+        print("Created empty table partitioned on column: {}".format(table.time_partitioning.field))
+    else:
+        print("Table already exists within dataset")
