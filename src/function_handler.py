7d17670427c3fb0fbb8867ff8069e4901c7c6801
Sung Won Chung <sungwon.chung@slalom.com>
2019-02-26 23:32:31 -0600

    updated function handler

--- src/function_handler.py
+++ src/function_handler.py
@@ -57,7 +57,9 @@ from lib.helper_functions import *
 from lib.infrastructure_setup import *
 
 #may want to define bigquery client, dataset_ref, and table_ref earlier in the handler function to avoid redundant code
+#TODO: add in flask requests module to work properly with cloud function
 def handler(schema_bq, schema_df):
+	"""Main function that orchestrates the data pipeline from start to finish"""
 	#define environment variables
 	project_id = 'iconic-range-220603' #capture the project id to where this data will land
 	bucket_name = 'chicago_traffic_raw' #capture bucket name where raw data will be stored
@@ -68,128 +70,37 @@ def handler(schema_bq, schema_df):
 	partition_by = '_last_updt' #partition by the last updated field for faster querying and incremental loads
 	timestamp = _getToday()
 	create_bucket(bucket_name)
-
-
-
-
-
-
-
-
-
-
-create_dataset_table(dataset_name, table_name, table_desc, schema_bq, partition_by)
-
-
-
-
-
-results_df = create_results_df()
-
-
-
-
-
-
-upload_raw_data_gcs(results_df, bucket_name)
-
-
-
-
-
-
-results_df_transformed = convert_schema(results_df, schema_df)
-
-print(results_df_transformed.dtypes)
-
-
-
-test_df = results_df_transformed.isnull().any()
-
-test_df
-
-
-
-#check if there are any nulls in the columns except for _comments
-
-
-
-
-
-null_columns = check_nulls(results_df_transformed)
-
-
-
-
-
-
-
-null_outliers = check_null_outliers(null_columns, nulls_expected)
-
-
-
-
-
-
-upload_to_gbq(results_df_transformed, project_id, dataset_name, table_name)
-
-
-
-
-
-
-bq_table_num_rows(dataset_name, table_name)
-
-
-
-# Create a query within cloud function to remove duplicates 
-#in another destination dataset/table, incremental load based on date
-# https://stackoverflow.com/questions/51475252/can-i-use-a-query-parameter-in-a-table-name
-#order of operations:
-#1. Get max timestamp from the raw table where timestamp>=today()
-#2. Select * from raw table > max timestamp and append to unique record table
-#3. Capture bq total number of rows
-
-
-
-    
-
-
-#check if it works
-query_max_timestamp(project_id, dataset_name, table_name)
-
-
-#create a table for unique records staging
-table_name_2 = 'unique_records_staging'
-table_desc_2 = "Unique records from table: {}".format(table_name)
-create_dataset_table(dataset_name, table_name_2, table_desc_2, schema_bq, partition_by)
-
-
-
-#create a table for unique records final
-table_name_2 = 'unique_records_final'
-table_desc_2 = "Unique records from table: {}".format(table_name)
-create_dataset_table(dataset_name, table_name_2, table_desc_2, schema_bq, partition_by)
-
-
-
-
-
-
-table_name_2 = "unique_records_staging"
-query_unique_records(project_id, dataset_name, table_name, table_name_2)
-
-
-
-
-
-
-table_name = 'unique_records_staging'
-table_name_2 = 'unique_records_final'
-append_unique_records(project_id, dataset_name, table_name, table_name_2)
-
-
-
-# return f'Uploaded raw csv file to bucket: {0}, and appended data to BigQuery dataset: {1}, table: {2} on '.format(bucket_name,dataset_name,table_name) + _getToday()
-
-
+	create_dataset_table(dataset_name, table_name, table_desc, schema_bq, partition_by)
+	results_df = create_results_df()
+	upload_raw_data_gcs(results_df, bucket_name)
+	results_df_transformed = convert_schema(results_df, schema_df)
+	print(results_df_transformed.dtypes)
+
+	#check if there are any nulls in the columns except for _comments
+	null_columns = check_nulls(results_df_transformed)
+	null_outliers = check_null_outliers(null_columns, nulls_expected)
+
+	#upload data to bigquery
+	upload_to_gbq(results_df_transformed, project_id, dataset_name, table_name)
+	bq_table_num_rows(dataset_name, table_name)
+
+	#Preprocess data for unique records accumulation
+	query_max_timestamp(project_id, dataset_name, table_name)
+	#create a table for unique records staging
+	table_name_2 = 'unique_records_staging'
+	table_desc_2 = "Unique records from table: {}".format(table_name)
+	create_dataset_table(dataset_name, table_name_2, table_desc_2, schema_bq, partition_by)
+	#create a table for unique records final
+	table_name_2 = 'unique_records_final'
+	table_desc_2 = "Unique records from table: {}".format(table_name)
+	create_dataset_table(dataset_name, table_name_2, table_desc_2, schema_bq, partition_by)
+	table_name_2 = "unique_records_staging"
+	query_unique_records(project_id, dataset_name, table_name, table_name_2)
+	table_name = 'unique_records_staging'
+	table_name_2 = 'unique_records_final'
+	append_unique_records(project_id, dataset_name, table_name, table_name_2)
+	#response body for api request
+	# return f'Uploaded raw csv file to bucket: {0}, and appended data to BigQuery dataset: {1}, table: {2} on '.format(bucket_name,dataset_name,table_name) + _getToday() TODO: this will be the response from the api
+
+if __name__ == "__main__":
+		handler()
