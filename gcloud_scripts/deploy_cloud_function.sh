68b6ccc62f5ec7be66defa0dfdbef656e0252166
Sung Won Chung <sungwon.chung@slalom.com>
2019-03-05 22:20:07 -0600

    created gcloud shell scripts

--- gcloud_scripts/deploy_cloud_function.sh
+++ gcloud_scripts/deploy_cloud_function.sh
@@ -1,1 +1,12 @@
-gcloud functions deploy test-1 --entry-point handler --runtime python37 --trigger-http
+
+#explain directory structure: https://cloud.google.com/functions/docs/writing/#structuring_source_code
+#tutorial for local cloud function deployment: https://cloud.google.com/functions/docs/deploying/filesystem
+
+#deploy function
+gcloud functions deploy test-sung --entry-point handler --runtime python37 --trigger-topic test_topic
+
+#publish message to pubsub topic trigger
+gcloud pubsub topics publish test_topic --message sung
+
+#check logs
+gcloud functions logs read --limit 20
