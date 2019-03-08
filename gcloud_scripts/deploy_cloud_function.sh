ff24074be90288594ecf09d1a620fc7a9e545188
Sung Won Chung <sungwon.chung@slalom.com>
2019-03-07 23:29:51 -0600

    updated for cloud scheduler gcloud cli

--- gcloud_scripts/deploy_cloud_function.sh
+++ gcloud_scripts/deploy_cloud_function.sh
@@ -1,6 +1,7 @@
 
 #explain directory structure: https://cloud.google.com/functions/docs/writing/#structuring_source_code
 #tutorial for local cloud function deployment: https://cloud.google.com/functions/docs/deploying/filesystem
+#all of this is run within cloud shell environment for linux/bash compatibility
 
 #deploy function and creates a pub/sub topic if it doesn't exist
 gcloud functions deploy test-sung --entry-point handler --runtime python37 --trigger-topic test_topic
@@ -12,11 +13,11 @@ gcloud pubsub topics publish test_topic --message "Can you see this?"
 gcloud functions logs read --limit 50
 
 #deploy cloud scheduler job to run every 5 minutes to test functio
-gcloud beta scheduler jobs create pubsub scheduled-function^
-	--schedule="*****"^
-	--topic=test_topic^
-	--message-body='{"Can you see this? With love, cloud scheduler"}'^
-	--time-zone='America/Chicago'
+gcloud beta scheduler jobs create pubsub scheduled-function \
+	--schedule "*/5 * * * *" \
+	--topic test_topic \
+	--message-body '{"Can you see this? With love, cloud scheduler"}' \
+	--time-zone 'America/Chicago'
 
 #manually run cloud scheduler job
-gcloud beta scheduler jobs run test_topic
+gcloud beta scheduler jobs run scheduled-function
