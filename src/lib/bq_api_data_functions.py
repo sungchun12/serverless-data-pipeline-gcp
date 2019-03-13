366a0994e05c6e4c2fe7f33821e473737a99f96f
Sung Won Chung <sungwon.chung@slalom.com>
2019-03-13 13:39:22 -0500

    updated tabs to 2

--- src/lib/bq_api_data_functions.py
+++ src/lib/bq_api_data_functions.py
@@ -7,25 +7,25 @@ Add a description here
 from google.cloud import bigquery
 
 def bq_table_num_rows(dataset_name, table_name):
-	"""Print total number of rows in destination bigquery table"""
-	bigquery_client = bigquery.Client() #instantiate bigquery client to interact with api
-	dataset_ref = bigquery_client.dataset(dataset_name) #create dataset object
-	table_ref = dataset_ref.table(table_name) #create table object 
-	table = bigquery_client.get_table(table_ref)  # API Request
-	num_rows = table.num_rows
-	print("Total number of rows: {0} in {1}".format(table.num_rows, table_ref.path))
-	return num_rows
+		"""Print total number of rows in destination bigquery table"""
+		bigquery_client = bigquery.Client() #instantiate bigquery client to interact with api
+		dataset_ref = bigquery_client.dataset(dataset_name) #create dataset object
+		table_ref = dataset_ref.table(table_name) #create table object 
+		table = bigquery_client.get_table(table_ref)  # API Request
+		num_rows = table.num_rows
+		print("Total number of rows: {0} in {1}".format(table.num_rows, table_ref.path))
+		return num_rows
 
 def query_max_timestamp(project_id, dataset_name, table_name):
-	"""Return the max timestamp from a table in bigquery after current date in CST"""
-	#in central Chicago time
-	sql = "SELECT max(_last_updt) as max_timestamp FROM `{0}.{1}.{2}` WHERE _last_updt >= TIMESTAMP(CURRENT_DATE('-06:00'))".format(project_id, dataset_name, table_name)
-	bigquery_client = bigquery.Client() #setup the client
-	query_job = bigquery_client.query(sql) #run the query
-	results = query_job.result() # waits for job to complete
-	for row in results: #returns the result
-		max_timestamp = row.max_timestamp.strftime('%Y-%m-%d %H:%M:%S')
-		return max_timestamp
+		"""Return the max timestamp from a table in bigquery after current date in CST"""
+		#in central Chicago time
+		sql = "SELECT max(_last_updt) as max_timestamp FROM `{0}.{1}.{2}` WHERE _last_updt >= TIMESTAMP(CURRENT_DATE('-06:00'))".format(project_id, dataset_name, table_name)
+		bigquery_client = bigquery.Client() #setup the client
+		query_job = bigquery_client.query(sql) #run the query
+		results = query_job.result() # waits for job to complete
+		for row in results: #returns the result
+				max_timestamp = row.max_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+				return max_timestamp
 
 # https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs#methods
 # https://googleapis.github.io/google-cloud-python/latest/bigquery/generated/google.cloud.bigquery.job.QueryJobConfig.html#google.cloud.bigquery.job.QueryJobConfig
@@ -35,36 +35,36 @@ def query_max_timestamp(project_id, dataset_name, table_name):
 # WRITE_APPEND: If the table already exists, BigQuery appends the data to the table. 
 # WRITE_EMPTY: If the table already exists and contains data, a 'duplicate' error is returned in the job result. 
 def query_unique_records(project_id, dataset_name, table_name, table_name_2):
-	"""Queries unique records from original table and saves results in staging table"""
-	bigquery_client = bigquery.Client()
-	job_config = bigquery.QueryJobConfig()
-	table_ref = bigquery_client.dataset(dataset_name).table(table_name_2) #set destination table
-	job_config.destination = table_ref
-	job_config.write_disposition = 'WRITE_TRUNCATE'
-	max_timestamp = query_max_timestamp(project_id, dataset_name, table_name)
-	sql = "SELECT * FROM `{0}.{1}.{2}` WHERE _last_updt >= TIMESTAMP(DATETIME '{3}');".format(project_id, dataset_name, table_name, max_timestamp)
-	query_job = bigquery_client.query(
-		sql,
-		# Location must match that of the dataset(s) referenced in the query
-		# and of the destination table.
-		location='US',
-		job_config=job_config)  # API request - starts the query
-	query_job.result()
-	print('Query results loaded to table {}'.format(table_ref.path))
+		"""Queries unique records from original table and saves results in staging table"""
+		bigquery_client = bigquery.Client()
+		job_config = bigquery.QueryJobConfig()
+		table_ref = bigquery_client.dataset(dataset_name).table(table_name_2) #set destination table
+		job_config.destination = table_ref
+		job_config.write_disposition = 'WRITE_TRUNCATE'
+		max_timestamp = query_max_timestamp(project_id, dataset_name, table_name)
+		sql = "SELECT * FROM `{0}.{1}.{2}` WHERE _last_updt >= TIMESTAMP(DATETIME '{3}');".format(project_id, dataset_name, table_name, max_timestamp)
+		query_job = bigquery_client.query(
+				sql,
+				# Location must match that of the dataset(s) referenced in the query
+				# and of the destination table.
+				location='US',
+				job_config=job_config)  # API request - starts the query
+		query_job.result()
+		print('Query results loaded to table {}'.format(table_ref.path))
 
 def append_unique_records(project_id, dataset_name, table_name, table_name_2):
-	"""Queries unique staging table and appends new results onto final table"""
-	bigquery_client = bigquery.Client()
-	job_config = bigquery.QueryJobConfig()
-	table_ref = bigquery_client.dataset(dataset_name).table(table_name_2) #set destination table
-	job_config.destination = table_ref
-	job_config.write_disposition = 'WRITE_APPEND'
-	sql = "SELECT a.* FROM `{0}.{1}.{2}` a LEFT JOIN `{0}.{1}.{3}` b on a.segmentid = b.segmentid AND a._last_updt = b._last_updt WHERE b.segmentid IS NULL AND b._last_updt IS NULL;".format(project_id, dataset_name, table_name, table_name_2)
-	query_job = bigquery_client.query(
-		sql,
-		# Location must match that of the dataset(s) referenced in the query
-		# and of the destination table.
-		location='US',
-		job_config=job_config)  # API request - starts the query
-	query_job.result()
-	print('Query results loaded to table {}'.format(table_ref.path))
+		"""Queries unique staging table and appends new results onto final table"""
+		bigquery_client = bigquery.Client()
+		job_config = bigquery.QueryJobConfig()
+		table_ref = bigquery_client.dataset(dataset_name).table(table_name_2) #set destination table
+		job_config.destination = table_ref
+		job_config.write_disposition = 'WRITE_APPEND'
+		sql = "SELECT a.* FROM `{0}.{1}.{2}` a LEFT JOIN `{0}.{1}.{3}` b on a.segmentid = b.segmentid AND a._last_updt = b._last_updt WHERE b.segmentid IS NULL AND b._last_updt IS NULL;".format(project_id, dataset_name, table_name, table_name_2)
+		query_job = bigquery_client.query(
+				sql,
+				# Location must match that of the dataset(s) referenced in the query
+				# and of the destination table.
+				location='US',
+				job_config=job_config)  # API request - starts the query
+		query_job.result()
+		print('Query results loaded to table {}'.format(table_ref.path))
--- src/lib/infrastructure_setup.py
+++ src/lib/infrastructure_setup.py
@@ -10,98 +10,98 @@ from google.cloud import bigquery
 #TODO: create a method where a service account is explicitly 
 # authorized to create a cloud function vs. using my local service account file
 def create_bucket(bucket_name):
-	"""Detects whether or not a new bucket needs to be created"""
-	client = storage.Client()
-	# .from_service_account_json('service_account.json') #authenticate service account
-	bucket = client.bucket(bucket_name) #capture bucket details
-	bucket.location = 'US-CENTRAL1' #define regional location
-	if not bucket.exists(): #checks if bucket doesn't exist
-		bucket.create()
-		print("Created a new bucket: {}".format(bucket.path))
-	else:
-		print("Bucket already exists: {}".format(bucket.path))
+		"""Detects whether or not a new bucket needs to be created"""
+		client = storage.Client()
+		# .from_service_account_json('service_account.json') #authenticate service account
+		bucket = client.bucket(bucket_name) #capture bucket details
+		bucket.location = 'US-CENTRAL1' #define regional location
+		if not bucket.exists(): #checks if bucket doesn't exist
+				bucket.create()
+				print("Created a new bucket: {}".format(bucket.path))
+		else:
+				print("Bucket already exists: {}".format(bucket.path))
 
 def dataset_exists(client, dataset_reference):
-	"""Return if a table exists.
+		"""Return if a table exists.
 
-	Args:
-			client (google.cloud.bigquery.client.Client):
-					A client to connect to the BigQuery API.
-			table_reference (google.cloud.bigquery.table.TableReference):
-					A reference to the table to look for.
+		Args:
+				client (google.cloud.bigquery.client.Client):
+						A client to connect to the BigQuery API.
+				table_reference (google.cloud.bigquery.table.TableReference):
+						A reference to the table to look for.
 
-	Returns:
-			bool: ``True`` if the table exists, ``False`` otherwise.
-	"""
-	from google.cloud.exceptions import NotFound
+		Returns:
+				bool: ``True`` if the table exists, ``False`` otherwise.
+		"""
+		from google.cloud.exceptions import NotFound
 
-	try:
-		client.get_dataset(dataset_reference)
-		return True
-	except NotFound:
-		return False
+		try:
+				client.get_dataset(dataset_reference)
+				return True
+		except NotFound:
+				return False
 
 def table_exists(client, table_reference):
-	"""Return if a table exists.
+		"""Return if a table exists.
 
-	Args:
-			client (google.cloud.bigquery.client.Client):
-					A client to connect to the BigQuery API.
-			table_reference (google.cloud.bigquery.table.TableReference):
-					A reference to the table to look for.
+		Args:
+				client (google.cloud.bigquery.client.Client):
+						A client to connect to the BigQuery API.
+				table_reference (google.cloud.bigquery.table.TableReference):
+						A reference to the table to look for.
 
-	Returns:
-			bool: ``True`` if the table exists, ``False`` otherwise.
-	"""
-	from google.cloud.exceptions import NotFound
+		Returns:
+				bool: ``True`` if the table exists, ``False`` otherwise.
+		"""
+		from google.cloud.exceptions import NotFound
 
-	try:
-		client.get_table(table_reference)
-		return True
-	except NotFound:
-		return False
+		try:
+				client.get_table(table_reference)
+				return True
+		except NotFound:
+				return False
 
 #https://cloud.google.com/bigquery/docs/python-client-migration#update_a_table
 def create_dataset_table(dataset_name, table_name, table_desc, schema, partition_by):
-	"""
-	Detects whether or not a new dataset and/or table need to be created.
-	Creates the dataset and table if either do not exist.
-	"""
-	#setup the client
-	bigquery_client = bigquery.Client()
+		"""
+		Detects whether or not a new dataset and/or table need to be created.
+		Creates the dataset and table if either do not exist.
+		"""
+		#setup the client
+		bigquery_client = bigquery.Client()
 
-	# Create a DatasetReference using a chosen dataset ID.
-	dataset_ref = bigquery_client.dataset(dataset_name) # The project defaults to the Client's project if not specified.
+		# Create a DatasetReference using a chosen dataset ID.
+		dataset_ref = bigquery_client.dataset(dataset_name) # The project defaults to the Client's project if not specified.
 
-	# Construct a full Dataset object to send to the API.
-	dataset = bigquery.Dataset(dataset_ref)
+		# Construct a full Dataset object to send to the API.
+		dataset = bigquery.Dataset(dataset_ref)
 
-	# Specify the geographic location where the dataset should reside.
-	dataset.location = "US"
-	
-	# Send the dataset to the API for creation.
-	# Raises google.api_core.exceptions.Conflict if the Dataset already exists within the project.
-	if dataset_exists(bigquery_client, dataset_ref) == False: #checks if dataset not found
-		dataset = bigquery_client.create_dataset(dataset)  # API request
-		print("Created new dataset: {}".format(dataset_ref.path))
-	else:
-		print("Dataset already exists: {}".format(dataset_ref.path))
-	
-	#Create an empty table
-	table_ref = dataset_ref.table(table_name) #construct a full table object to send to the api
-	
-	if table_exists(bigquery_client, table_ref) == False: #checks if table not found
-		table = bigquery.Table(table_ref, schema=schema)
-		table.time_partitioning = bigquery.TimePartitioning(
-				type_=bigquery.TimePartitioningType.DAY, #day is the only supported type for now
-				field=partition_by)  # name of column to use for partitioning
-		table = bigquery_client.create_table(table)  # API request
-		assert table.table_id == table_name #checks if table_id matches table_name
+		# Specify the geographic location where the dataset should reside.
+		dataset.location = "US"
 		
-		#update the table description
-		table.description = table_desc
-		table = bigquery_client.update_table(table, ['description'])  # API request
-		assert table.description == table_desc #checks if table description matches the update
-		print("Created empty table partitioned on column: {}".format(table.time_partitioning.field))
-	else:
-		print("Table already exists: {}".format(table_ref.path))
+		# Send the dataset to the API for creation.
+		# Raises google.api_core.exceptions.Conflict if the Dataset already exists within the project.
+		if dataset_exists(bigquery_client, dataset_ref) == False: #checks if dataset not found
+				dataset = bigquery_client.create_dataset(dataset)  # API request
+				print("Created new dataset: {}".format(dataset_ref.path))
+		else:
+				print("Dataset already exists: {}".format(dataset_ref.path))
+		
+		#Create an empty table
+		table_ref = dataset_ref.table(table_name) #construct a full table object to send to the api
+		
+		if table_exists(bigquery_client, table_ref) == False: #checks if table not found
+				table = bigquery.Table(table_ref, schema=schema)
+				table.time_partitioning = bigquery.TimePartitioning(
+						type_=bigquery.TimePartitioningType.DAY, #day is the only supported type for now
+						field=partition_by)  # name of column to use for partitioning
+				table = bigquery_client.create_table(table)  # API request
+				assert table.table_id == table_name #checks if table_id matches table_name
+			
+				#update the table description
+				table.description = table_desc
+				table = bigquery_client.update_table(table, ['description'])  # API request
+				assert table.description == table_desc #checks if table description matches the update
+				print("Created empty table partitioned on column: {}".format(table.time_partitioning.field))
+		else:
+				print("Table already exists: {}".format(table_ref.path))
