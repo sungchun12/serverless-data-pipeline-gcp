34d051c8f41089bccd2fdfa4a4978539190fda11
Sung Won Chung <sungwon.chung@slalom.com>
2019-03-13 16:51:41 -0500

    drafted all stack traces

--- src/main.py
+++ src/main.py
@@ -37,59 +37,65 @@ def handler(event, context):
     		project_id=project_id, transport=BackgroundThreadTransport)
 		# instantiate tracer
 		tracer = tracer_module.Tracer(exporter=exporter)
+
 		with tracer.span(name='get_kpis') as span_get_kpis:
 				#prints a message from the pubsub trigger
 				pubsub_message = base64.b64decode(event['data']).decode('utf-8')
 				print(pubsub_message) #can be used to configure dynamic pipeline creation
 
-		with span_get_kpis.span(name='infrastructure_var_setup'):
-				#TODO: create a class for all these objects? yes
-				#https://dbader.org/blog/6-things-youre-missing-out-on-by-never-using-classes-in-your-python-code
-				#define infrastructure variables
-				bucket_name = 'chicago_traffic_raw' #capture bucket name where raw data will be stored
-				dataset_name = 'chicago_traffic_demo' #initial dataset
-				table_name = 'traffic_raw' #name of table to capture data
-				table_desc = 'Raw, public Chicago traffic data is appended to this table every 5 minutes'#table description
-				table_staging = 'traffic_staging'
-				table_staging_desc = "Unique records greater than or equal to current date from table: {}".format(table_name)
-				table_final = 'traffic_final'
-				table_final_desc = "Unique, historical records accumulated from table: {}".format(table_name)
-				nulls_expected = ('_comments') #tuple of nulls expected for checking data outliers
-				partition_by = '_last_updt' #partition by the last updated field for faster querying and incremental loads
-				from lib.schemas import schema_bq, schema_df #import schemas
+				with span_get_kpis.span(name='infrastructure_var_setup'):
+						#TODO: create a class for all these objects? yes
+						#https://dbader.org/blog/6-things-youre-missing-out-on-by-never-using-classes-in-your-python-code
+						#define infrastructure variables
+						bucket_name = 'chicago_traffic_raw' #capture bucket name where raw data will be stored
+						dataset_name = 'chicago_traffic_demo' #initial dataset
+						table_name = 'traffic_raw' #name of table to capture data
+						table_desc = 'Raw, public Chicago traffic data is appended to this table every 5 minutes'#table description
+						table_staging = 'traffic_staging'
+						table_staging_desc = "Unique records greater than or equal to current date from table: {}".format(table_name)
+						table_final = 'traffic_final'
+						table_final_desc = "Unique, historical records accumulated from table: {}".format(table_name)
+						nulls_expected = ('_comments') #tuple of nulls expected for checking data outliers
+						partition_by = '_last_updt' #partition by the last updated field for faster querying and incremental loads
+						from lib.schemas import schema_bq, schema_df #import schemas
 
-		with span_get_kpis.span(name='infrastructure_creation') as span_infra_create:
-				#create infrastructure
-				with span_infra_create.span(name='bucket_creation'):
-						create_bucket(bucket_name)
-				with span_infra_create.span(name='dataset_table_creation'):
-						create_dataset_table(dataset_name, table_name, table_desc, schema_bq, partition_by) #create raw table
-						create_dataset_table(dataset_name, table_staging, table_staging_desc, schema_bq, partition_by) #create a table for unique records staging
-						create_dataset_table(dataset_name, table_final, table_final_desc, schema_bq, partition_by) #create a table for unique records final
+				with span_get_kpis.span(name='infrastructure_creation') as span_infra_create:
+						#create infrastructure
+						with span_infra_create.span(name='bucket_creation'):
+								create_bucket(bucket_name)
+						with span_infra_create.span(name='dataset_table_creation'):
+								create_dataset_table(dataset_name, table_name, table_desc, schema_bq, partition_by) #create raw table
+								create_dataset_table(dataset_name, table_staging, table_staging_desc, schema_bq, partition_by) #create a table for unique records staging
+								create_dataset_table(dataset_name, table_final, table_final_desc, schema_bq, partition_by) #create a table for unique records final
 
-		with span_get_kpis.span(name='ingest_raw_data') as span_ingest_raw:
-				with span_ingest_raw.span(name='create_dataframe'):
-						#access data from API, create dataframe, and upload raw csv
-						results_df = create_results_df()
-				with span_ingest_raw.span(name='upload_raw_data_gcs'):
-						upload_raw_data_gcs(results_df, bucket_name)
+				with span_get_kpis.span(name='ingest_raw_data') as span_ingest_raw:
+						with span_ingest_raw.span(name='create_dataframe'):
+								#access data from API, create dataframe, and upload raw csv
+								results_df = create_results_df()
+						with span_ingest_raw.span(name='upload_raw_data_gcs'):
+								upload_raw_data_gcs(results_df, bucket_name)
 
-		with span_get_kpis.span(name='convert_schema'):
-				#perform schema conversion on dataframe to match bigquery schema
-				results_df_transformed = convert_schema(results_df, schema_df)
-				print(results_df_transformed.dtypes)
+				with span_get_kpis.span(name='convert_schema'):
+						#perform schema conversion on dataframe to match bigquery schema
+						results_df_transformed = convert_schema(results_df, schema_df)
+						print(results_df_transformed.dtypes)
 
-				#check if there are any nulls in the columns and print exceptions
-				null_columns = check_nulls(results_df_transformed)
-				null_outliers = check_null_outliers(null_columns, nulls_expected)
+				with span_get_kpis.span(name='audit_null_columns'):
+						#check if there are any nulls in the columns and print exceptions
+						null_columns = check_nulls(results_df_transformed)
+						null_outliers = check_null_outliers(null_columns, nulls_expected)
 
-				#upload data to bigquery
-				upload_to_gbq(results_df_transformed, project_id, dataset_name, table_name)
-				bq_table_num_rows(dataset_name, table_name)
+				with span_get_kpis.span(name='upload_to_gbq'):
+						#upload data to bigquery
+						upload_to_gbq(results_df_transformed, project_id, dataset_name, table_name)
+						bq_table_num_rows(dataset_name, table_name)
 
-				#Preprocess data for unique records accumulation
-				query_unique_records(project_id, dataset_name, table_name, table_staging)
-				bq_table_num_rows(dataset_name, table_staging)
-				append_unique_records(project_id, dataset_name, table_staging, table_final)
-				bq_table_num_rows(dataset_name, table_final)
-				print("Data Pipeline Fully Realized!")
+				with span_get_kpis.span(name='preprocess_data') as span_prep_data:
+						#Preprocess data for unique records accumulation
+						with span_prep_data.span(name='query_unique_records'):
+								query_unique_records(project_id, dataset_name, table_name, table_staging)
+								bq_table_num_rows(dataset_name, table_staging)
+						with span_prep_data.span(name='append_unique_records'):
+								append_unique_records(project_id, dataset_name, table_staging, table_final)
+								bq_table_num_rows(dataset_name, table_final)
+						print("Data Pipeline Fully Realized!")
