7f91152027c8474155779812eec074f13456b3f8
Sung Won Chung <sungwon.chung@slalom.com>
2019-04-01 12:29:28 -0500

    updated for pep8 line lengths

--- src/lib/bq_api_data_functions.py
+++ src/lib/bq_api_data_functions.py
@@ -16,8 +16,8 @@ def bq_table_num_rows(dataset_name, table_name):
     bigquery_client = (
         bigquery.Client()
     )  # instantiate bigquery client to interact with api
-    dataset_ref = bigquery_client.dataset(dataset_name)  # create dataset object
-    table_ref = dataset_ref.table(table_name)  # create table object
+    dataset_ref = bigquery_client.dataset(dataset_name)  # create dataset obj
+    table_ref = dataset_ref.table(table_name)  # create table obj
     table = bigquery_client.get_table(table_ref)  # API Request
     num_rows = table.num_rows
     logger.info(f"Total number of rows: {table.num_rows} in {table_ref.path}")
@@ -25,9 +25,13 @@ def bq_table_num_rows(dataset_name, table_name):
 
 
 def query_max_timestamp(project_id, dataset_name, table_name):
-    """Return the max timestamp from a table in bigquery after current date in CST"""
+    """Return the max timestamp
+    from a table in bigquery after current date in CST
+    """
     # in central Chicago time
-    sql = f"SELECT max(_last_updt) as max_timestamp FROM `{project_id}.{dataset_name}.{table_name}` WHERE _last_updt >= TIMESTAMP(CURRENT_DATE('-06:00'))"
+    sql = f"SELECT max(_last_updt) as max_timestamp \
+            FROM `{project_id}.{dataset_name}.{table_name}` \
+            WHERE _last_updt >= TIMESTAMP(CURRENT_DATE('-06:00'))"
     bigquery_client = bigquery.Client()  # setup the client
     query_job = bigquery_client.query(sql)  # run the query
     results = query_job.result()  # waits for job to complete
@@ -40,11 +44,12 @@ def query_max_timestamp(project_id, dataset_name, table_name):
 # https://googleapis.github.io/google-cloud-python/latest/bigquery/generated/google.cloud.bigquery.job.QueryJobConfig.html#google.cloud.bigquery.job.QueryJobConfig
 # The following values are supported:
 # configuration.copy.writeDisposition
-# WRITE_TRUNCATE: If the table already exists, BigQuery overwrites the table data.
-# WRITE_APPEND: If the table already exists, BigQuery appends the data to the table.
-# WRITE_EMPTY: If the table already exists and contains data, a 'duplicate' error is returned in the job result.
+# WRITE_TRUNCATE: If the table already exists, BigQuery overwrites the table
+# WRITE_APPEND: If the table already exists, BigQuery appends the data
+# WRITE_EMPTY: If the table already exists and contains data,
+# a 'duplicate' error is returned in the job result.
 def query_unique_records(project_id, dataset_name, table_name, table_name_2):
-    """Queries unique records from original table and saves results in staging table"""
+    """Queries unique records from original table, saves in staging table"""
     bigquery_client = bigquery.Client()
     job_config = bigquery.QueryJobConfig()
     table_ref = bigquery_client.dataset(dataset_name).table(
@@ -53,7 +58,8 @@ def query_unique_records(project_id, dataset_name, table_name, table_name_2):
     job_config.destination = table_ref
     job_config.write_disposition = "WRITE_TRUNCATE"
     max_timestamp = query_max_timestamp(project_id, dataset_name, table_name)
-    sql = f"SELECT * FROM `{project_id}.{dataset_name}.{table_name}` WHERE _last_updt >= TIMESTAMP(DATETIME '{max_timestamp}');"
+    sql = f"SELECT * FROM `{project_id}.{dataset_name}.{table_name}` \
+            WHERE _last_updt >= TIMESTAMP(DATETIME '{max_timestamp}');"
     query_job = bigquery_client.query(
         sql,
         # Location must match that of the dataset(s) referenced in the query
@@ -74,7 +80,10 @@ def append_unique_records(project_id, dataset_name, table_name, table_name_2):
     )  # set destination table
     job_config.destination = table_ref
     job_config.write_disposition = "WRITE_APPEND"
-    sql = f"SELECT a.* FROM `{project_id}.{dataset_name}.{table_name}` a LEFT JOIN `{project_id}.{dataset_name}.{table_name_2}` b on a.segmentid = b.segmentid AND a._last_updt = b._last_updt WHERE b.segmentid IS NULL;"
+    sql = f"SELECT a.* FROM `{project_id}.{dataset_name}.{table_name}` a \
+            LEFT JOIN `{project_id}.{dataset_name}.{table_name_2}` b \
+            on a.segmentid = b.segmentid AND a._last_updt = b._last_updt \
+            WHERE b.segmentid IS NULL;"
     query_job = bigquery_client.query(
         sql,
         # Location must match that of the dataset(s) referenced in the query
